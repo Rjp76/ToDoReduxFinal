f545126c76ec10339859870f461317691d705ae4
Rati Patel <31673893+Rjp76@users.noreply.github.com>
2018-05-08 13:52:10 -0400

    Add ToDo Reducer added

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+function todoApp(state,action){
+    switch (action.type){
+        case 'ADD_TODO':
+            var newState = Object.assign({},state);
+            newState.todo.items.push({
+                message: action.message,
+                completed: false
+            });
+    }
+}
