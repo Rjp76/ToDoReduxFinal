a11c3444350f7e3459abdf05c48d54990e562c40
Rati Patel <31673893+Rjp76@users.noreply.github.com>
2018-05-08 14:01:26 -0400

    Complete, delete and clear Reducers added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+
 function todoApp(state,action){
     switch (action.type){
         case 'ADD_TODO':
@@ -6,5 +7,35 @@ function todoApp(state,action){
                 message: action.message,
                 completed: false
             });
+            return newState;
+        case 'COMPLETE_TODO':
+            var newState =Object.assign({},state);
+            if (newState.todo.items[action.index].completed){
+                newState.todo.items[action.index].completed = false;
+            }else {
+                newState.todo.items[action.index].completed = true;
+            }
+
+
+            return newState;
+
+        case 'DELETE_TODO':
+            var items = [].concat(state.todo.items);
+            items.splice(action.index,1);
+            return Object.assign({},state,{
+                todo:{
+                    items:items
+                }
+            });
+        case 'CLEAR_TODO':
+            return Object.assign({},state,{
+                todo:{
+                    items:[]
+                }
+            });
+        default:
+            return state;
+
     }
 }
+export default todoApp;
